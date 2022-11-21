ec293cdf12f9edd129beae87073bd9d4a6b1f357
Bryan Hite <bhite2@mail.usf.edu>
2022-11-21 12:14:15 -0500

    Updated name of robot. Added to robot attack

--- robot.py
+++ robot.py
@@ -2,9 +2,10 @@ from weapon import Weapon
 
 class Robot:
     def __init__(self, name):
-        self.name = name
+        self.name = "Bender"
         self.health = 100
         self.weapon = Weapon("flamethrower", 40)
     
     def attack(self, dinosaur):
-        dino.health -= self.weapon.attack_power
+        dinosaur.health -= self.weapon.attack_power
+        print(f"{self.name} attacks {dinosaur.name} with {self.weapon.name} for {self.weapon.attack_power} damage. {dinosaur.name} remaining health is {dinosaur.health}.")

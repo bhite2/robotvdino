d214546f7e3a92db8a768145f4a1ed31e6103f76
Bryan Hite <bhite2@mail.usf.edu>
2022-11-21 12:18:38 -0500

    Added to Dinosaur attack on robot

--- dino.py
+++ dino.py
@@ -1,8 +1,9 @@
 class Dinosaur:
     def __init__(self):
-        self.name = ""
-        self.health = ()
-        self.attack_power = ()
+        self.name = "Barney"
+        self.health = (100)
+        self.attack_power = (20)
         
-    def attack_robot(self, name, attack_power):
-        
+    def attack_robot(self, robot, attack_power):
+        robot.health -= self.attack_power
+        print(f"{self.name} attacks {robot.name} for {self.attack_power} damage. {robot.name} new health is {robot.health}.")

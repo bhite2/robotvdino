80bc0e1eadce16e83a8f981b483845db37968ae0
Bryan Hite <bhite2@mail.usf.edu>
2022-11-26 11:01:13 -0500

    Completed welcome display for start of the game.

--- battlefield.py
+++ battlefield.py
@@ -4,21 +4,25 @@ from robot import Robot
 class Battlefield:
     def __init__(self):
         self.robot = Robot("Bender")
-        self.dinosaur = Dinosaur()
-        pass
+        self.dinosaur = Dinosaur("Barney")
+        
     def run_game(self):
         self.display_welcome()
         self.battle_phase()
         self.display_winner()
 
     def display_welcome(self):
-        print("Welcome to Robot vs. Dinosaur")
-        print("Who will reign supreme? Past or Future?")
+        print("Ladies and gentlemen. Welcome to the main event of the evening!")
+        print("This is Robot vs. Dinosaur")
+        print("A battle, millions of years in the making")
+        print("To the thousands in attendance, and the millions watching at home")
+        print("Let's get Ready to Rumbleeeeeeeeeeee!")
 
     def battle_phase(self):
         # I will need a loop that stops at which character gets to zero first
         self.robot.attack(self.dinosaur)
-        pass
+        self.dinosaur.attack_robot(self.robot)
+    
 
     def display_winner(self):
         pass
--- dino.py
+++ dino.py
@@ -1,6 +1,6 @@
 class Dinosaur:
-    def __init__(self):
-        self.name = "Barney"
+    def __init__(self, name):
+        self.name = name
         self.health = 100
         self.attack_power = 20
         
